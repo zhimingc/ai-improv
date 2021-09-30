24b373352da1dec86eccafb64c278b8d77b52512
Zhiming Chen <zhi.m.ch@gmail.com>
2021-09-30 16:05:21 +0800

    Added TTS for game and prompt requests

--- Main/GameController.gd
+++ Main/GameController.gd
@@ -2,17 +2,27 @@ extends Control
 
 enum PROMPT_TYPE { EMOTION, LOCATION, OCCUPATION, RELATIONSHIP }
 
+# Short form games
+var gameList = ["Toaster", "Forward, Reverse", "Questions Only", "Eye contact",
+				"Whoosh", "Bad Dub (Gibberish)", "Stand, sit, lie", "Character Swap"]
+
 export var prompt_pool_size = 5
 
 var promptType
 var promptTypePool = [0, 1, 2, 3]
-var requesting = false
+var requesting = [false, false]
+
+# tts variables
+var queueSpeak = false
+var gameTTS = ""
+var promptTTS = ""
+
+var currentGame = 0
 
 func _ready():
 	$HTTPRequest.connect("request_completed", self, "_on_prompt_request_completed")
 
 func _on_prompt_request_completed(result, response_code, headers, body):
-	# var json = JSON.parse(body.get_string_from_utf8())
 	var xml = XMLParser.new()
 	var xmlError = xml.open_buffer(body)
 	var xmlRead = 0
@@ -28,18 +38,55 @@ func _on_prompt_request_completed(result, response_code, headers, body):
 	var prompt = prompts[rand_range(0, prompts.size())]
 	$ShowPanel2.set_word(prompt)
 	$ShowPanel2.set_label("Prompt (" + promptType + ")")
-	requesting = false
+	requesting[0] = false
+	promptTTS = "Your prompt is " + promptType + ", " + prompt
+
+func tts_speak(text):
+	$tts.speak(text)
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_select") and promptTypePool.size() > 0 and requesting == false:
+	if Input.is_action_just_pressed("new_prompt") and promptTypePool.size() > 0 and requesting[0] == false:
 		request_prompt(promptTypePool[rand_range(0, promptTypePool.size())])
+		queueSpeak = true
+	
+	if Input.is_action_just_pressed("new_game"):
+		get_rand_game()
+		queueSpeak = true
+		
+	update_tts()
+
+func get_rand_game():
+	$ShowPanel.set_word("thinking...")
+	$ShowPanel.set_label("")
+	var nextGame = currentGame
+	while nextGame == currentGame:
+		nextGame = rand_range(0, gameList.size())
+	requesting[1] = true
+	
+	yield(get_tree().create_timer(rand_range(0.5, 1.5)), "timeout")
+	$ShowPanel.set_word(gameList[nextGame])
+	$ShowPanel.set_label("Game")
+	gameTTS = "Your next game is " + gameList[nextGame]
+	requesting[1] = false
 
 func request_prompt(type):
 	promptType = str(PROMPT_TYPE.keys()[type]).to_lower()
 	var result = $HTTPRequest.request("https://improbot.com/?key=" + promptType + "&n=" + str(prompt_pool_size) + "&lang=en")
 	$ShowPanel2.set_word("thinking...")
 	$ShowPanel2.set_label("")
-	requesting = true
+	requesting[0] = true
+
+func update_tts():
+	if not queueSpeak:
+		return
+	var canSpeak = true
+	for request in requesting:
+		if request == true:
+			canSpeak = false
+			
+	if canSpeak:
+		$tts.speak(gameTTS + " and " + promptTTS)
+		queueSpeak = false
 	
 func _on_debug_prompt_pressed(prompt):
 	var index = promptTypePool.find(prompt)
--- Panels/ClockPanel.gd
+++ Panels/ClockPanel.gd
@@ -1,6 +1,9 @@
 extends TextureRect
 
+signal timer_reset
+
 export var startTime = 60.0
+export var timeRange = Vector2(5.0, 10.0)
 
 var currentTime = 0.0
 
@@ -10,3 +13,10 @@ func _ready():
 func _process(delta):
 	currentTime -= delta
 	$ClockText.bbcode_text = "[center]" + String(currentTime).pad_decimals(0)
+
+	if currentTime <= 0.0:
+		reset_timer()
+
+func reset_timer():
+	currentTime = rand_range(timeRange.x, timeRange.y)
+	emit_signal("timer_reset")
