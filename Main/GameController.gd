d96fa32ceb7d011af5a8204c2412bd0960bddc44
Zhiming Chen <zhi.m.ch@gmail.com>
2021-10-19 06:54:10 +0800

    Added show state machine and pauses between games

--- Main/GameController.gd
+++ Main/GameController.gd
@@ -1,18 +1,24 @@
 extends Control
 
-class Game:	
-	func _init(_name, type):
-		gameName = _name
-		gamePace = type
-
-	var gameName = ""
-	var gamePace = PACE.FLEXIBLE
-
 signal clock_start
 
 enum PACE { SLOW, FAST, FLEXIBLE }
 enum PROMPT_TYPE { EMOTION = 0, LOCATION, OCCUPATION, RELATIONSHIP, WORD }
-enum GAMESTATE { IDLE, POST_GAME, SELECTION, PRE_GAME }
+enum GAMESTATE { PRE_SHOW, IN_GAME, POST_GAME, SELECTION, PRE_GAME, POST_SHOW }
+
+class Game:	
+	var GAME_REQ = ["a bell", "chairs"]
+	
+	func _init(_name, type, reqs = []):
+		gameName = _name
+		gamePace = type
+		for req in reqs:
+			if req <= GAME_REQ.size():
+				requirements.append(GAME_REQ[req])
+
+	var gameName = ""
+	var gamePace = PACE.FLEXIBLE
+	var requirements = []
 
 export var prompt_pool_size = 5
 export var net_mode = false
@@ -20,46 +26,62 @@ export var net_mode = false
 var offlinePromptPaths = ["Prompts/prompt-emotion.txt", "Prompts/prompt-location.txt", "Prompts/prompt-occupation.txt", 
 						"Prompts/prompt-relationship.txt", "Prompts/prompt-word.txt"]
 
+# state machine
+var currentState = GAMESTATE.PRE_SHOW
+
 # Short form games
 var gameList = ["Toaster", "Forward, Reverse", "Questions Only", "Eye contact",
 				"Whoosh", "Bad Dub", "Stand, sit, lie", "Character Swap",
 				"Open Scenes", "One Syllable", "Causal Carousel"]
 var games = []
+var originalGames = []
 var currentGame
 var currentGameId = 0
 
 var promptType = ""
+var promptLabel = []
 var promptEnum = PROMPT_TYPE.EMOTION
 var promptTypePool = [0, 1, 2, 3, 4]
-var requesting = [false, false]
 
 # tts variables
-var queueSpeak = false
 var gameTTS = ""
 var promptTTS = ""
+var speakQueue = []
 
 # timer
 var clockReset = false
 
 func init_games():
-	games.append(Game.new("Toaster", PACE.SLOW))
-	games.append(Game.new("Forward, Reverse", PACE.SLOW))
-	games.append(Game.new("Questions Only", PACE.FAST))
-	games.append(Game.new("Eye Contact", PACE.FLEXIBLE))
+	games.append(Game.new("Toaster", PACE.SLOW, [0, 1]))
+	games.append(Game.new("Forward, Reverse", PACE.SLOW, [0]))
+	games.append(Game.new("Questions Only", PACE.FAST, []))
+	games.append(Game.new("Eye Contact", PACE.FLEXIBLE, [0]))
 	games.append(Game.new("Whoosh", PACE.FLEXIBLE))
 	games.append(Game.new("Bad Dub", PACE.FLEXIBLE))
 	games.append(Game.new("Stand sit lie", PACE.FLEXIBLE))
-	games.append(Game.new("Character Swap", PACE.SLOW))
+	games.append(Game.new("Character Swap", PACE.SLOW, [0]))
 	games.append(Game.new("Open Scenes", PACE.SLOW))
 	games.append(Game.new("One Syllable", PACE.FAST))
-	games.append(Game.new("Causal Carousel", PACE.SLOW))
+	games.append(Game.new("Alphabet", PACE.FAST))
+	games.append(Game.new("Change, +1, More", PACE.FLEXIBLE, [0]))
+	# games.append(Game.new("Causal Carousel", PACE.SLOW))
+	# games.append(Game.new("Blind Lines", PACE.SLOW))
+	originalGames = games
+
+func reset_game_list():
+	games = originalGames
+
+func remove_game(index):
+	games.remove(index)
+	if games.size() == 0:
+		reset_game_list()
 
 func _ready():
 	randomize()
 	$HTTPRequest.connect("request_completed", self, "_on_prompt_request_completed")
 	init_games()
-	full_request()
-	
+	set_state(GAMESTATE.PRE_SHOW)
+
 func _on_prompt_request_completed(result, response_code, headers, body):
 	var xml = XMLParser.new()
 	var xmlError = xml.open_buffer(body)
@@ -77,7 +99,7 @@ func _on_prompt_request_completed(result, response_code, headers, body):
 	set_prompt_label(prompt)
 	
 func offline_prompt_request():
-	yield(get_tree().create_timer(rand_range(0.5, 1.5)), "timeout")	
+	# yield(get_tree().create_timer(rand_range(0.5, 1.5)), "timeout")	
 	var prompt = read_offline_prompt()
 	set_prompt_label(prompt)
 	
@@ -91,52 +113,128 @@ func read_offline_prompt():
 	return prompt
 
 func set_prompt_label(prompt):
-	$ShowPanel2.set_word(prompt)
-	$ShowPanel2.set_label("Prompt (" + promptType + ")")
-	requesting[0] = false
-	promptTTS = "Your prompt is " + promptType + ", " + prompt
+	promptLabel = [prompt, "Prompt (" + promptType + ")"]
+	promptTTS = "Your prompt is " + prompt	
+
+func give_prompt():
+	set_game_texts($ShowPanel2, promptLabel)	
+	speakQueue.append(promptTTS)
+
+func get_rand_game():
+	set_game_texts($ShowPanel, ["thinking...", ""])
+	
+	var nextGame = currentGameId
+	while nextGame == currentGameId:
+		nextGame = rand_range(0, games.size())
+	
+	yield(get_tree().create_timer(rand_range(0.25, 0.5)), "timeout")
+	
+	currentGameId = nextGame
+	currentGame = games[nextGame]
+	remove_game(nextGame)
+	$ShowPanel.set_word(currentGame.gameName)
+	$ShowPanel.set_label("Game")
+	set_game_texts($ShowPanel, [currentGame.gameName, "Game"])
+	gameTTS = "Your next game is " + currentGame.gameName + ". "
+	if currentGame.requirements.size() > 0:
+		var reqs = "You will need "
+		for req in currentGame.requirements:
+			reqs += req + ','
+		gameTTS += reqs
+	speakQueue.append(gameTTS)
+
+func set_game_texts(label, words):
+	label.set_word(words[0])
+	label.set_label(words[1])
 
 func tts_speak(text):
 	$tts.speak(text)
 
 func _process(delta):
-	if promptTypePool.size() > 0 and requesting[0] == false:
+	update_debug()
+	update_state()
+	update_tts()
+
+func update_debug():
+	if promptTypePool.size() > 0:
 		if Input.is_action_just_pressed("new_prompt"):
 			request_prompt(promptTypePool[rand_range(0, promptTypePool.size())])
-			queueSpeak = true
-	
-	if Input.is_action_just_pressed("new_game"):
-		get_rand_game()
-		queueSpeak = true
-		
-	if clockReset:
-		clockReset = false
-		full_request()
+			give_prompt()
 
-	update_tts()
+	if Input.is_action_just_pressed("new_game"):
+		# get_rand_game()
+		$ClockPanel.zero_timer()
+	
+func update_state():
+	match currentState:
+		GAMESTATE.PRE_SHOW:	
+			pass
+		GAMESTATE.PRE_GAME:
+			pass
+		GAMESTATE.IN_GAME:
+			pass
+		GAMESTATE.POST_GAME:
+			pass
+		GAMESTATE.POST_SHOW:
+			if not $tts._get_is_speaking():
+				tts_speak("Show Over, Go Home.")
+			
+func set_state(newState):
+	# on exit
+	match currentState:
+		GAMESTATE.PRE_SHOW:			
+			pass
+		GAMESTATE.PRE_GAME:
+			pass
+		GAMESTATE.IN_GAME:
+			pass
+		GAMESTATE.POST_GAME:
+			pass
+	currentState = newState
+	# on enter
+	match currentState:
+		GAMESTATE.PRE_SHOW:
+			speakQueue.append("The show will start in 5. 4.... 3. 2.... 1.")
+			set_game_texts($ShowPanel, ["", ""])
+			set_game_texts($ShowPanel2, ["", ""])
+			$ClockPanel.set_manual_timer(7.0)
+			pass
+		GAMESTATE.PRE_GAME:
+			get_rand_game()
+			request_prompt(promptTypePool[rand_range(0, promptTypePool.size())])			
+			set_game_texts($ShowPanel2, ["", ""])
+			$ClockPanel.set_manual_timer(10.0)
+			pass
+		GAMESTATE.IN_GAME:
+			reset_clock()
+			emit_signal("clock_start")
+			give_prompt()			
+			pass
+		GAMESTATE.POST_GAME:
+			speakQueue.append("SCEEENE!... That was really funny ha, ha, ha, ha, ha.")	
+			set_game_texts($ShowPanel, ["SCENE", ""])
+			set_game_texts($ShowPanel2, ["SCENE", ""])
+			$ClockPanel.set_manual_timer(10.0)			
+			pass
+
+func _on_ClockPanel_timer_reset():
+	match currentState:
+		GAMESTATE.PRE_SHOW:
+			set_state(GAMESTATE.PRE_GAME)	
+			pass
+		GAMESTATE.PRE_GAME:
+			set_state(GAMESTATE.IN_GAME)
+			pass
+		GAMESTATE.IN_GAME:
+			set_state(GAMESTATE.POST_GAME)
+			pass
+		GAMESTATE.POST_GAME:
+			set_state(GAMESTATE.PRE_GAME)
+			pass
 
 func full_request():
 	request_prompt(promptTypePool[rand_range(0, promptTypePool.size())])
 	get_rand_game()	
-	queueSpeak = true
-
-func get_rand_game():
-	$ShowPanel.set_word("thinking...")
-	$ShowPanel.set_label("")
-	var nextGame = currentGameId
-	while nextGame == currentGameId:
-		nextGame = rand_range(0, games.size())
-	requesting[1] = true
-	
-	yield(get_tree().create_timer(rand_range(0.5, 1.5)), "timeout")
-	
-	currentGameId = nextGame
-	currentGame = games[nextGame]
-	$ShowPanel.set_word(currentGame.gameName)
-	$ShowPanel.set_label("Game")
-	gameTTS = "Your next game is " + currentGame.gameName
-	requesting[1] = false
-	reset_clock()
 
 func reset_clock():
 	$ClockPanel.set_new_timer(currentGame)
@@ -144,27 +242,17 @@ func reset_clock():
 func request_prompt(type):
 	promptEnum = type
 	promptType = str(PROMPT_TYPE.keys()[type]).to_lower()
-	$ShowPanel2.set_word("thinking...")
-	$ShowPanel2.set_label("")
+	set_game_texts($ShowPanel2, ["thinking...", ""])
 	if net_mode:
 		var result = $HTTPRequest.request("https://improbot.com/?key=" + promptType + "&n=" + str(prompt_pool_size) + "&lang=en")
 	else:
 		offline_prompt_request()
-	requesting[0] = true
 
 func update_tts():
-	if not queueSpeak:
-		return
-	var canSpeak = true
-	for request in requesting:
-		if request == true:
-			canSpeak = false
-			
-	if canSpeak:
-		$tts.speak(gameTTS + " and " + promptTTS)
-		queueSpeak = false
-		yield(get_tree().create_timer(5.0), "timeout")
-		emit_signal("clock_start")
+	if not $tts._get_is_speaking() and speakQueue.size() > 0:
+		var toSpeak = speakQueue[0]
+		tts_speak(toSpeak)
+		speakQueue.remove(0)
 	
 func _on_debug_prompt_pressed(prompt):
 	var index = promptTypePool.find(prompt)
@@ -173,22 +261,12 @@ func _on_debug_prompt_pressed(prompt):
 	else:
 		promptTypePool.remove(index)
 
-
-func _on_ClockPanel_timer_reset():
-	clockReset = true
-
 func _on_ClockPanel_timer_ending():
-	$tts.speak("You have five seconds left.")
+	speakQueue.append("You have ten seconds left.")
 	
 func _on_ClockPanel_show_over():
-	$ShowPanel.set_word("")
+	$ShowPanel.set_word("Show")
 	$ShowPanel.set_label("")
-	$ShowPanel2.set_word("")
+	$ShowPanel2.set_word("Over")
 	$ShowPanel2.set_label("")
-	$ClockPanel.clockActive = false
-	$ClockPanel/ClockText.bbcode_text = "[center]Show Over"
-	$tts.stop()
-	$tts.speak("Show over, go home.")
-	# yield(get_tree().create_timer(1.0), "timeout")
-	# _on_ClockPanel_show_over()
-	
+	set_state(GAMESTATE.POST_SHOW)
--- Panels/ClockPanel.gd
+++ Panels/ClockPanel.gd
@@ -5,18 +5,21 @@ signal timer_ending
 signal show_over
 
 export var debugTimer = false
+export var debugTime = 15.0
 export var totalTime = 300.0
-export var startTime = 60.0
-export var warningTime = 5.0
+export var increasePace = 6.0
+export var warningTime = 10.0
 export var timeRange = [Vector2(60.0, 120.0), Vector2(180.0, 240.0)]
 
 var timerBarObj
 var currentTime = 0.0
-var clockActive = true
-var warned = false
+var clockActive = false
+var giveWarning = false
 
 func _ready():
-	currentTime = startTime
+	totalTime *= 60
+	increasePace *= 60
+	currentTime = 10
 	timerBarObj = get_parent().get_node("TimerBar")
 
 func _process(delta):
@@ -25,20 +28,28 @@ func _process(delta):
 		timerBarObj.update_timebar(currentTime)
 	
 		if totalTime <= 0.0:
-			emit_signal("show_over")
 			totalTime = 0.0
+			clockActive = false
+			$ClockText.bbcode_text = ""
+			$TotalTime.bbcode_text = "[center]0.0"
+			emit_signal("show_over")
+			return
 		else:
 			totalTime -= delta
 			
 		if currentTime <= 0.0:
 			reset_timer()
-			currentTime = 0.0
-		if currentTime < warningTime and not warned:
+		if currentTime < warningTime and giveWarning:
+			giveWarning = false			
 			emit_signal("timer_ending")
-			warned = true
 
-	$ClockText.bbcode_text = "[center]" + String(currentTime/60).pad_decimals(0) + ":" + String(floor(fmod(currentTime, 60.0))).pad_decimals(0)
-	$TotalTime.bbcode_text = "[center]" + String(totalTime/60).pad_decimals(0) + ":" + String(floor(fmod(totalTime, 60.0))).pad_decimals(0)
+		$ClockText.bbcode_text = "[center]" + String(currentTime/60).pad_decimals(0) + ":" + String(floor(fmod(currentTime, 60.0))).pad_decimals(0)
+		$TotalTime.bbcode_text = "[center]" + String(totalTime/60).pad_decimals(0) + ":" + String(floor(fmod(totalTime, 60.0))).pad_decimals(0)
+
+func set_manual_timer(time):
+	currentTime = time	
+	timerBarObj.set_new_time(currentTime)
+	clockActive = true
 
 func set_new_timer(game):
 	match game.gamePace:
@@ -50,17 +61,21 @@ func set_new_timer(game):
 			currentTime = rand_range(timeRange[0].x, timeRange[1].y)
 	currentTime = stepify(currentTime, 5)
 	clockActive = false
-	warned = false
+	giveWarning = true
 
 	if debugTimer:
-		currentTime = 10
+		currentTime = debugTime
 		clockActive = true
 	
 	timerBarObj.set_new_time(currentTime)
 	
+func zero_timer():
+	currentTime = 0.0
+	
 func reset_timer():
+	clockActive = false	
+	currentTime = 0.0			
 	emit_signal("timer_reset")
-	clockActive = false
 
 func _on_AIController_clock_start():
 	clockActive = true
